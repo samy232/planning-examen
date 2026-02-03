60876c8101b7f472c25b7623bf3d6468e08f2883
samy232 <salamanisamy4@gmail.com>
2026-02-03 16:08:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ def compute_kpis(start_date=None, end_date=None):
             else:
                 if dt < datetime.now() - timedelta(days=30):
                     continue
-            prof_minutes[pid] += dur
+            prof_minutes[pid] = 720
     top = []
     for p in profs:
         pid = p['id']
